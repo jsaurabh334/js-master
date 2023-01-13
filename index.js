b72c55bd336e37fac78717ff3f2dd4796703ce37
jsaurabh334 <jsaurabh334@gmail.com>
2023-01-13 18:04:57 +0530

    final

--- index.js
+++ index.js
@@ -3,11 +3,45 @@
 // 1.for loop
 // 2.while
 // 3. do while  
-// wap to print table of 4 
-let i = 1
+// // wap to print table of 4 
+// var a = "saurabh"
+//  var age = 23
+// //  saurabh is 23 yrs old 
+// console.log(a , "is", age , "yrs old")
+// // template literals 
+// console.log(`${a} is ${age} yrs old`)
 
-var number  = 4
-do {
-    console.log(i *4 )
-    i++
-} while (i<=10);
+// 3 == "buzz" 5 == fizz
+
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 == 0 || index % 5==0) {
+       if (index%3==0) {
+        console.log("buzz")
+        
+       }
+       else{
+        console.log("fizz")
+       }
+        
+    }
+    else{
+        console.log(index)
+    }
+
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
